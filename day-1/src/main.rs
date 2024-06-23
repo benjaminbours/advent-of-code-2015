3df0ac9e641e0a21bb977369ee7ec6c6d40fc481
Benjamin Bours <boursbenjamin@gmail.com>
2024-06-23 15:08:17 +0100

    Day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -12,5 +13,26 @@ fn part_1() {
         _ => acc,
     });
 
-    println!("Result: {}", result);
+    println!("Part 1 result: {}", result);
+}
+
+fn part_2() {
+    let input = fs::read_to_string("./day-1/input.txt").unwrap();
+    let mut floor = 0;
+    let mut position = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        }
+
+        if floor == -1 {
+            position = i + 1;
+            break;
+        }
+    }
+
+    println!("Part 2 result: {}", position);
 }
