85c699b79c44e61d835c598af937d7ed0d69f4f9
Benjamin Bours <boursbenjamin@gmail.com>
2024-06-23 20:10:43 +0100

    Day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -27,4 +28,38 @@ fn part_1() {
         .len();
 
     println!("Part 1 result: {} unique houses", unique_houses);
+}
+
+fn part_2() {
+    let input = fs::read_to_string("./day-3/input.txt").unwrap();
+    let mut santa_history = vec![(0, 0)];
+    let mut robot_santa_history = vec![(0, 0)];
+
+    for (i, c) in input.chars().enumerate() {
+        let history = if i % 2 == 0 {
+            &mut santa_history
+        } else {
+            &mut robot_santa_history
+        };
+
+        let (x, y) = history.last().unwrap().clone();
+        let (new_x, new_y) = match c {
+            '^' => (x, y + 1),
+            'v' => (x, y - 1),
+            '>' => (x + 1, y),
+            '<' => (x - 1, y),
+            _ => (x, y),
+        };
+
+        history.push((new_x, new_y));
+    }
+
+    santa_history.extend(robot_santa_history);
+
+    let unique_houses = santa_history
+        .iter()
+        .collect::<std::collections::HashSet<_>>()
+        .len();
+
+    println!("Part 2 result: {} unique houses", unique_houses);
 }
