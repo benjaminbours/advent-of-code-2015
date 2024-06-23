b90201cc63a6c28c6153c9074a332cfb01dd7930
Benjamin Bours <boursbenjamin@gmail.com>
2024-06-23 20:28:39 +0100

    Day 4 part 1

---
+++ day-4/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    part_1();
+}
+
+fn part_1() {
+    let input = fs::read_to_string("./day-4/input.txt").unwrap();
+
+    // (0..) is an infinite iterator that starts at 0
+    let result = (0..)
+        .find(|i| {
+            let digest = md5::compute(format!("{}{}", input, i));
+            // :x is the format specifier for hexadecimal
+            format!("{:x}", digest).starts_with("00000")
+        })
+        .unwrap();
+
+    println!("Part 1 result: {}", result);
+}
